8dfb862d9c8885ccd9934c390bc0fdb769aaa2d3
luna <223230@proton.me>
2024-02-19 22:39:36 +0100

    Document the Oscilloscope

--- src/editor/views/oscilloscope.rs
+++ src/editor/views/oscilloscope.rs
@@ -4,6 +4,96 @@ use nih_plug_vizia::vizia::{prelude::*, vg};
 
 use crate::utils::PeakWaveformRingBuffer;
 
+/// A peak Oscilloscope.
+///
+/// Takes in a PeakWaveformRingBuffer to display the waveform, retaining peak
+/// details for all frequencies within the sample rate, regardless of buffer
+/// size.
+///
+/// # How to use
+///
+/// To use this Visualizer, you need a PeakWaveformBuffer. It also needs to be
+/// made thread-safe so that you can send it to the editor thread. So, usually,
+/// you'll need to follow these steps:
+///
+/// - Add a an `Arc<Mutex<PeakWaveformRingBuffer<f32, SIZE>>>` to your plugin struct (providing your own `SIZE` - typically, anything from 500 to 5000 is fine)
+/// - Initialize the buffer by calling `set_sample_rate()` on it inside your plugin's `initialize()` function
+/// - Push samples into the buffer inside your plugin's `process()` function
+/// - Send a `clone()` of the `Arc<Mutex>` to the editor thread via `editor::create()`
+///
+/// Then, inside the editor's `create()` function, you'll only need to:
+///
+/// - Call `Oscilloscope::new()` and pass the `Arc` into it.
+///
+/// # Example
+///
+/// Here's a (somewhat opinionated) and very long example. Note that this
+/// example only pushes the left channel data into the oscilloscope.
+///
+/// ## Plugin code (`lib.rs`)
+///
+/// ```no_run
+/// pub struct YourPlugin {
+///     oscilloscope_buffer: Arc<Mutex<PeakWaveformRingBuffer<f32, 1024>>>,
+///     ...
+/// }
+///
+/// impl Default for YourPlugin {
+///     fn default() -> Self {
+///         Self {
+///             oscilloscope_buffer: Arc::new(Mutex::new(PeakWaveformRingBuffer::new(44100., 20.))),
+///             ...
+///         }
+///     }
+/// }
+///
+/// impl Plugin for YourPlugin {
+///     fn process(
+///        &mut self,
+///        buffer: &mut Buffer,
+///        _aux: &mut AuxiliaryBuffers,
+///        _context: &mut impl ProcessContext<Self>,
+///     ) -> ProcessStatus {
+///         ...
+///         for sample in buffer.iter_samples()[0] {
+///             self.oscilloscope_buffer
+///                 .lock()
+///                 .unwrap()
+///                 .enqueue(sample.clone());
+///         }
+///         ...
+///     }
+///
+///     fn editor(&mut self, _async_executor: AsyncExecutor<Self>) -> Option<Box<dyn Editor>> {
+///         editor::create(
+///             editor::Data {
+///                 oscilloscope_buffer: self.oscilloscope_buffer.clone(),
+///             },
+///             ...
+///         )
+///     }
+///
+///     ...
+///
+/// }
+/// ```
+///
+/// ## Editor code (`lib.rs`)
+///
+/// ```no_run
+/// #[derive(Lens, Clone)]
+/// pub(crate) struct Data {
+///     pub(crate) oscilloscope_buffer: Arc<Mutex<PeakWaveformRingBuffer<f32, 1024>>>,
+///     ...
+/// }
+///
+/// pub(crate) fn create(editor_data: Data, editor_state: Arc<ViziaState>) -> Option<Box<dyn Editor>> {
+///     create_vizia_editor(editor_state, ViziaTheming::default(), move |cx, _| {
+///         editor_data.clone().build(cx);
+///         Oscilloscope::new(cx, Data::post_buffer);
+///     })
+/// }
+/// ```
 pub struct Oscilloscope<B, const BUFFER_SIZE: usize>
 where
     B: Lens<Target = Arc<Mutex<PeakWaveformRingBuffer<f32, BUFFER_SIZE>>>>,
