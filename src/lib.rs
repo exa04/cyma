e36644e6f62d1a07c30eb850a7bcdf40890b5499
luna <223230@proton.me>
2024-03-16 21:11:41 +0100

    Shorten top-level doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 //! Views and associated data structures for plug-in UIs made using
 //! [nih-plug](https://github.com/robbert-vdh/nih-plug) and
-//! [VIZIA](https://github.com/vizia/vizia). Plext contains many views that you
-//! can use to quickly make good-looking plug-in UIs using VIZIA.
+//! [VIZIA](https://github.com/vizia/vizia).
 
 pub mod utils;
 pub mod visualizers;
