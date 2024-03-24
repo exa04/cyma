3ae0f70419279ed495c18484a51eeab2d4bf60e1
luna <223230@proton.me>
2024-03-24 11:15:18 +0100

    Base `Graph` on generic type instead of a concrete buffer

--- src/visualizers/graph.rs
+++ src/visualizers/graph.rs
@@ -1,6 +1,9 @@
-use std::sync::{Arc, Mutex};
+use len_trait::len::Len;
+use std::{
+    ops::Index,
+    sync::{Arc, Mutex},
+};
 
-use crate::utils::PeakBuffer;
 use nih_plug_vizia::vizia::{
     binding::{Lens, LensExt, Res},
     context::{Context, DrawContext},
@@ -9,23 +12,26 @@ use nih_plug_vizia::vizia::{
     views::normalized_map::amplitude_to_db,
 };
 
-/// A graph showing real-time peak information.
-pub struct PeakGraph<B>
+/// A real-time graph displaying information that is stored inside a iterable
+/// collection.
+pub struct Graph<L, I>
 where
-    B: Lens<Target = Arc<Mutex<PeakBuffer<f32>>>>,
+    L: Lens<Target = Arc<Mutex<I>>>,
+    I: Len + Index<usize, Output = f32>,
 {
-    buffer: B,
+    buffer: L,
     display_range: (f32, f32),
     scale_by_db: bool,
 }
 
-impl<B> PeakGraph<B>
+impl<L, I> Graph<L, I>
 where
-    B: Lens<Target = Arc<Mutex<PeakBuffer<f32>>>>,
+    L: Lens<Target = Arc<Mutex<I>>>,
+    I: Len + Index<usize, Output = f32> + 'static,
 {
     pub fn new(
         cx: &mut Context,
-        buffer: B,
+        buffer: L,
         display_range: impl Res<(f32, f32)>,
         scale_by_db: impl Res<bool>,
     ) -> Handle<Self> {
@@ -38,9 +44,10 @@ where
     }
 }
 
-impl<B> View for PeakGraph<B>
+impl<L, I> View for Graph<L, I>
 where
-    B: Lens<Target = Arc<Mutex<PeakBuffer<f32>>>>,
+    L: Lens<Target = Arc<Mutex<I>>>,
+    I: Len + Index<usize, Output = f32> + 'static,
 {
     fn element(&self) -> Option<&'static str> {
         Some("22-visualizer")
@@ -59,50 +66,57 @@ where
         let mut stroke = vg::Path::new();
         let binding = self.buffer.get(cx);
         let ring_buf = &(binding.lock().unwrap());
-        let mut rb_iter = ring_buf.into_iter();
 
-        let mut i = 0.;
+        let mut i = 1;
+
         if self.scale_by_db {
-            let mut peak = (amplitude_to_db(*(rb_iter.next().unwrap())))
-                .clamp(self.display_range.0, self.display_range.1);
+            let mut peak =
+                (amplitude_to_db(ring_buf[0])).clamp(self.display_range.0, self.display_range.1);
 
             peak -= self.display_range.0;
             peak /= self.display_range.1 - self.display_range.0;
 
             stroke.move_to(x, y + h * (1. - peak));
 
-            for p in rb_iter {
+            while i < ring_buf.len() {
                 // Convert peak to decibels and clamp it in range
-                peak = (amplitude_to_db(*p)).clamp(self.display_range.0, self.display_range.1);
+                peak = (amplitude_to_db(ring_buf[i]))
+                    .clamp(self.display_range.0, self.display_range.1);
 
                 // Normalize peak's range
                 peak -= self.display_range.0;
                 peak /= self.display_range.1 - self.display_range.0;
 
                 // Draw peak as a new point
-                stroke.line_to(x + (w / ring_buf.len() as f32) * i, y + h * (1. - peak));
-                i += 1.;
+                stroke.line_to(
+                    x + (w / ring_buf.len() as f32) * i as f32,
+                    y + h * (1. - peak),
+                );
+                i += 1;
             }
         } else {
             let mut peak =
-                (*(rb_iter.next().unwrap())).clamp(self.display_range.0, self.display_range.1);
+                (amplitude_to_db(ring_buf[0])).clamp(self.display_range.0, self.display_range.1);
 
             peak -= self.display_range.0;
             peak /= self.display_range.1 - self.display_range.0;
 
             stroke.move_to(x, y + h * (1. - peak));
 
-            for peak in rb_iter {
+            while i < ring_buf.len() {
                 // Clamp peak in range
-                let mut peak = (*peak).clamp(self.display_range.0, self.display_range.1);
+                let mut peak = ring_buf[i].clamp(self.display_range.0, self.display_range.1);
 
                 // Normalize peak's range
                 peak -= self.display_range.0;
                 peak /= self.display_range.1 - self.display_range.0;
 
                 // Draw peak as a new point
-                stroke.line_to(x + (w / ring_buf.len() as f32) * i, y + h * (1. - peak));
-                i += 1.;
+                stroke.line_to(
+                    x + (w / ring_buf.len() as f32) * i as f32,
+                    y + h * (1. - peak),
+                );
+                i += 1;
             }
         }
 
