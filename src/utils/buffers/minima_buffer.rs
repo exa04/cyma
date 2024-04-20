0aca686baea39569c5dcc1bdd97da870b901f5f4
luna <223230@proton.me>
2024-04-20 18:40:58 +0200

    Document buffers

--- src/utils/buffers/minima_buffer.rs
+++ src/utils/buffers/minima_buffer.rs
@@ -1,13 +1,20 @@
+use nih_plug::audio_setup::{AudioIOLayout, BufferConfig};
+use nih_plug::prelude::InitContext;
 use std::ops::{Index, IndexMut};
 
 use super::{RingBuffer, VisualizerBuffer};
 
-/// Analogous to the `PeakBuffer`, save for the fact that it stores the minimum
-/// absolute values instead of the maximum absolute values of a signal over time.
+/// Analogous to the [`PeakBuffer`](super::PeakBuffer), save for the fact that it
+/// stores the minimum absolute values instead of the maximum absolute values of a
+/// signal over time.
 ///
 /// This buffer is useful for gain reduction meters / graphs. For regular
 /// visualizers that are meant to display peak information, such as peak graphs, do
 /// use a `PeakBuffer`.
+///
+/// The `MinimaBuffer` needs to be provided a sample rate after initialization - do
+/// this inside your [`initialize()`](nih_plug::plugin::Plugin::initialize)
+/// function.
 #[derive(Clone, Default)]
 pub struct MinimaBuffer {
     buffer: RingBuffer<f32>,
@@ -27,6 +34,15 @@ pub struct MinimaBuffer {
 }
 
 impl MinimaBuffer {
+    /// Constructs a new `MinimaBuffer`.
+    ///
+    /// * `size` - The length of the buffer in samples; Usually, this can be kept < 2000
+    /// * `duration` - The duration (in seconds) of the audio data inside the buffer
+    /// * `decay` - The time it takes for a sample inside the buffer to decrease by -12dB, in milliseconds
+    ///
+    /// The buffer needs to be provided a sample rate after initialization - do this by
+    /// calling [`set_sample_rate`](Self::set_sample_rate) inside your
+    /// [`initialize()`](nih_plug::plugin::Plugin::initialize) function.
     pub fn new(size: usize, duration: f32, decay: f32) -> Self {
         let decay_weight = Self::decay_weight(decay, size, duration);
         Self {
@@ -41,17 +57,46 @@ impl MinimaBuffer {
         }
     }
 
+    /// Sets the decay time of the `MinimaBuffer`.
+    ///
+    /// * `decay` - The time it takes for a sample inside the buffer to decrease by -12dB, in milliseconds
     pub fn set_decay(self: &mut Self, decay: f32) {
         self.decay = decay;
         self.update();
     }
 
+    /// Sets the sample rate of the incoming audio.
+    ///
+    /// This function **clears** the buffer. You can call it inside your
+    /// [`initialize()`](nih_plug::plugin::Plugin::initialize) function and provide the
+    /// sample rate like so:
+    ///
+    /// ```
+    /// fn initialize(
+    ///     &mut self,
+    ///     _audio_io_layout: &AudioIOLayout,
+    ///     buffer_config: &BufferConfig,
+    ///     _context: &mut impl InitContext<Self>,
+    /// ) -> bool {
+    ///     match self.minima_buffer.lock() {
+    ///         Ok(mut buffer) => {
+    ///             buffer.set_sample_rate(buffer_config.sample_rate);
+    ///         }
+    ///         Err(_) => return false,
+    ///     }
+    ///
+    ///     true
+    /// }
+    /// ```
     pub fn set_sample_rate(self: &mut Self, sample_rate: f32) {
         self.sample_rate = sample_rate;
         self.update();
         self.buffer.clear();
     }
 
+    /// Sets the duration (in seconds) of the incoming audio.
+    ///
+    /// This function **clears** the buffer.
     pub fn set_duration(self: &mut Self, duration: f32) {
         self.duration = duration;
         self.update();
--- src/utils/buffers/waveform_buffer.rs
+++ src/utils/buffers/waveform_buffer.rs
@@ -4,23 +4,19 @@ use std::ops::{Index, IndexMut};
 
 use super::VisualizerBuffer;
 
-/// A special type of ring buffer, intended for use in peak waveform analysis.
+/// A special type of ring buffer for waveform analysis.
 ///
 /// This is a wrapper around the [`RingBuffer`](crate::utils::RingBuffer) struct
-/// that specifically handles waveforms. It stores elements of type T in pairs
-/// to represent the minimum and maximum values of a waveform over a certain
-/// interval. It provides methods for setting the sample rate and duration, as
-/// well as enqueueing new values and retrieving the stored waveform data.
+/// that handles waveforms. It stores elements of type T in pairs, to represent the
+/// minimum and maximum values of a waveform over a certain interval.
 ///
-/// For each pair `(T,T)` of samples that a WaveformBuffer holds, the first
-/// element is the local minimum, and the second is the local maximum within the
-/// respective time frame.
+/// For each pair `(T,T)` of samples that a WaveformBuffer holds, the first element
+/// is the local minimum, and the second is the local maximum within the respective
+/// time frame.
 ///
-/// ![Alt version](http://127.0.0.1:5500/img.svg)
-///
-/// These values can be used to construct a zoomed-out representation of the
-/// audio data without losing peak information - which is why this buffer is
-/// used in the [`Oscilloscope`](crate::editor::views::Oscilloscope).
+/// These values can be used to construct a zoomed-out representation of the audio
+/// data without losing peak information - which is why this buffer is used in the
+/// [`Oscilloscope`](crate::editor::views::Oscilloscope).
 #[derive(Clone, PartialEq, Default)]
 pub struct WaveformBuffer {
     buffer: RingBuffer<(f32, f32)>,
@@ -37,8 +33,14 @@ pub struct WaveformBuffer {
 }
 
 impl WaveformBuffer {
-    /// Creates a new `WaveformBuffer` with the specified sample rate and
-    /// duration (in seconds).
+    /// Constructs a new `WaveformBuffer`
+    ///
+    /// * `size` - The length of the buffer in samples; Usually, this can be kept < 2000
+    /// * `duration` - The duration (in seconds) of the audio data inside the buffer
+    ///
+    /// The buffer needs to be provided a sample rate after initialization - do this by
+    /// calling [`set_sample_rate`](Self::set_sample_rate) inside your
+    /// [`initialize()`](nih_plug::plugin::Plugin::initialize) function.
     pub fn new(size: usize, sample_rate: f32, duration: f32) -> Self {
         let sample_delta = Self::sample_delta(size, sample_rate as f32, duration as f32);
         Self {
@@ -51,14 +53,39 @@ impl WaveformBuffer {
             t: sample_delta,
         }
     }
-    /// Sets the sample rate of the buffer and **clears** it.
+
+    /// Sets the sample rate of the incoming audio.
+    ///
+    /// This function **clears** the buffer. You can call it inside your
+    /// [`initialize()`](nih_plug::plugin::Plugin::initialize) function and provide the
+    /// sample rate like so:
+    ///
+    /// ```
+    /// fn initialize(
+    ///     &mut self,
+    ///     _audio_io_layout: &AudioIOLayout,
+    ///     buffer_config: &BufferConfig,
+    ///     _context: &mut impl InitContext<Self>,
+    /// ) -> bool {
+    ///     match self.waveform_buffer.lock() {
+    ///         Ok(mut buffer) => {
+    ///             buffer.set_sample_rate(buffer_config.sample_rate);
+    ///         }
+    ///         Err(_) => return false,
+    ///     }
+    ///
+    ///     true
+    /// }
+    /// ```
     pub fn set_sample_rate(self: &mut Self, sample_rate: f32) {
         self.sample_rate = sample_rate;
         self.sample_delta = Self::sample_delta(self.buffer.len(), sample_rate, self.duration);
         self.buffer.clear();
     }
 
-    /// Sets the duration of the buffer (in seconds) and **clears** it.
+    /// Sets the duration (in seconds) of the incoming audio.
+    ///
+    /// This function **clears** the buffer.
     pub fn set_duration(self: &mut Self, duration: f32) {
         self.duration = duration;
         self.sample_delta = Self::sample_delta(self.buffer.len(), self.sample_rate, duration);
@@ -116,6 +143,7 @@ impl VisualizerBuffer<f32> for WaveformBuffer {
         self.buffer.clear();
     }
 
+    /// Grows the buffer, **clearing it**.
     fn grow(self: &mut Self, size: usize) {
         if size == self.buffer.len() {
             return;
@@ -125,6 +153,7 @@ impl VisualizerBuffer<f32> for WaveformBuffer {
         self.buffer.clear();
     }
 
+    /// Shrinks the buffer, **clearing it**.
     fn shrink(self: &mut Self, size: usize) {
         if size == self.buffer.len() {
             return;
